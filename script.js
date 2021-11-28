9cad11716f56891f40b1176757e24a9a7555296b
KarasovRuslan <karasov.ruslan@icloud.com>
2021-11-28 07:55:54 +0200

    HW8 commit

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function clocksTime() {
     let hours = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours());
     let minutes = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
     let seconds = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
-    
+
     return isFullFormat ? `${hours} : ${minutes} : ${seconds}` : `${hours} : ${minutes}`;
 };
 
